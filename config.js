4321d69e62c82ce4dd0614ce0293153b04770692
Mikael Brevik <mikaelb@mikaelb.net>
2012-01-19 17:19:24 +0100

    Fixed typos

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ exports.GITHUB_REGEX    = /http(?:s?):\/\/github\.com\/([a-zA-Z0-9\_\-]+)\/([a-z
 
 // MongoDB Settings
 // mongodb://heroku_app2530248:hkas3dm8ibo3te8op6cv81ima6@ds029797.mongolab.com:29797/heroku_app2530248
-export.DB_URL 			= "ds029797.mongolab.com:29797";
-export.DB_NAME 			= "heroku_app2530248";
-export.DB_USER 			= "heroku_app2530248";
-export.DB_PASS 			= "hkas3dm8ibo3te8op6cv81ima6";
+exports.DB_URL 			= "ds029797.mongolab.com:29797";
+exports.DB_NAME 			= "heroku_app2530248";
+exports.DB_USER 			= "heroku_app2530248";
+exports.DB_PASS 			= "hkas3dm8ibo3te8op6cv81ima6";
