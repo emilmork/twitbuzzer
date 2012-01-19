f5054cf01f824bdba1d2beb7f4980b8d6754e634
Mikael Brevik <mikaelb@mikaelb.net>
2012-01-19 17:17:48 +0100

    Added config file

---
+++ config.js
@@ -0,0 +1,11 @@
+
+exports.SPOTIFY_REGEX   = /http(?:s?):\/\/open\.spotify\.com\/([a-z]+)\/([a-zA-Z0-9]{22})/;
+exports.GITHUB_REGEX    = /http(?:s?):\/\/github\.com\/([a-zA-Z0-9\_\-]+)\/([a-zA-Z0-9\_\-]+)\/?/;
+
+
+// MongoDB Settings
+// mongodb://heroku_app2530248:hkas3dm8ibo3te8op6cv81ima6@ds029797.mongolab.com:29797/heroku_app2530248
+export.DB_URL 			= "ds029797.mongolab.com:29797";
+export.DB_NAME 			= "heroku_app2530248";
+export.DB_USER 			= "heroku_app2530248";
+export.DB_PASS 			= "hkas3dm8ibo3te8op6cv81ima6";
