1e4eb8a10a27c32dc266f6541b49145455c79b63
Mikael Brevik <mikaelb@mikaelb.net>
2012-01-20 11:32:56 +0100

    Testing mapReduce instead of grouping

--- config.js
+++ config.js
@@ -1,11 +1,19 @@
 
-exports.SPOTIFY_REGEX   = /http(?:s?):\/\/open\.spotify\.com\/([a-z]+)\/([a-zA-Z0-9]{22})/;
-exports.GITHUB_REGEX    = /http(?:s?):\/\/github\.com\/([a-zA-Z0-9\_\-]+)\/([a-zA-Z0-9\_\-]+)\/?/;
+exports.app = {
+	// Regular expressions to recognize tweets
+	SPOTIFY_REGEX: /http(?:s?):\/\/open\.spotify\.com\/([a-z]+)\/([a-zA-Z0-9]{22})/,
+	GITHUB_REGEX: /http(?:s?):\/\/github\.com\/([a-zA-Z0-9\_\-]+)\/([a-zA-Z0-9\_\-]+)\/?/,
 
+	// MongoDB Settings
+	// mongodb://heroku_app2530248:hkas3dm8ibo3te8op6cv81ima6@ds029797.mongolab.com:29797/heroku_app2530248
+	DB_URL: "ds029797.mongolab.com:29797",
+	DB_NAME: "heroku_app2530248",
+	DB_USER: "heroku_app2530248",
+	DB_PASS: "hkas3dm8ibo3te8op6cv81ima6",
 
-// MongoDB Settings
-// mongodb://heroku_app2530248:hkas3dm8ibo3te8op6cv81ima6@ds029797.mongolab.com:29797/heroku_app2530248
-exports.DB_URL 			= "ds029797.mongolab.com:29797";
-exports.DB_NAME 			= "heroku_app2530248";
-exports.DB_USER 			= "heroku_app2530248";
-exports.DB_PASS 			= "hkas3dm8ibo3te8op6cv81ima6";
+	// Twitter Settings
+	consumer_key: 'd45dmERqPcgbhXUXtXciQ',
+	consumer_secret: 'YVw7xoMzVC6p7RNrTsTFcd2VPxQN0oTnilcHmH8cs',
+	access_token_key: '40382389-AWpD5CVziGPIrrvTFmNp6YffLrEShUvzNQ1iRQuc8',
+	access_token_secret: 'Zz39fgkdezr7ZX0xOPdIR7pf7zrpKkYz3S9CEk1E'
+}
--- twitbuzzer.js
+++ twitbuzzer.js
@@ -1,15 +1,15 @@
-var express = require('express'),
-    app = express.createServer(),
-    twitter = require('ntwitter'),
-    io  = require('socket.io').listen(app),
-    spotify = require('./spotify.js'),
-    github = require('./github.js'),
-    mongoose = require('mongoose'),
-    config = require('./config');
+var express     = require('express'),
+    app         = express.createServer(),
+    twitter     = require('ntwitter'),
+    io          = require('socket.io').listen(app),
+    spotify     = require('./lib/spotify'),
+    github      = require('./lib/github'),
+    config      = require('./config'),
+    mongodb     = require('./lib/mongodb');
 
 
 // Start connection to MongoDb
-initializeDb();
+mongodb.initializeDb();
 
 // Heroku Socket.IO support. 
 // assuming io is the Socket.IO server object
@@ -23,10 +23,10 @@ io.configure(function () {
 });
 
 var twit = new twitter({
-  consumer_key: 'd45dmERqPcgbhXUXtXciQ',
-  consumer_secret: 'YVw7xoMzVC6p7RNrTsTFcd2VPxQN0oTnilcHmH8cs',
-  access_token_key: '40382389-AWpD5CVziGPIrrvTFmNp6YffLrEShUvzNQ1iRQuc8',
-  access_token_secret: 'Zz39fgkdezr7ZX0xOPdIR7pf7zrpKkYz3S9CEk1E'
+  consumer_key:  config.app.consumer_key,
+  consumer_secret:  config.app.consumer_secret,
+  access_token_key:  config.app.access_token_key,
+  access_token_secret: config.app.access_token_secret
 });
 
 app.listen(process.env.PORT || 40825);
@@ -41,18 +41,25 @@ app.get('/', function (req, res) {
 app.use("/css", express.static(__dirname + '/web/css'));
 app.use("/js", express.static(__dirname + '/web/js'));
 
-app.use("/test", function (req, res) {
+// Serve API for fetching generated Github Repo objects
+app.use("/github/:daylimit/:limit/:page?", function (req, res) {
     res.contentType('application/json');
-    mapReduce("twitbuzzer", function (err, data) {
+    mongodb.mapReduce("twitbuzzer", req.params.daylimit, req.params.limit, req.params.page, function (err, data) {
         if (err) console.log(err);
         
         res.send(JSON.stringify(data));
     });
 });
 
+// Listen to twitter stream data.
 twit.stream('statuses/filter', {'track':'github,spotify'}, function(stream) {
     stream.on('data', handleStream());
 });
+
+/*****************************************************
+ *                Tweet Stream handling              *
+ *               And sending through IO!             *
+ *****************************************************/
 
 function handleStream () {
 
@@ -60,8 +67,8 @@ function handleStream () {
 
         if(data.entities.urls.length < 1) return;
 
-        var githubData  = getGithubID(data.entities.urls),
-            spotifyData = getSpotifyID(data.entities.urls);
+        var githubData  = getGithubID(data.entities.urls);
+        // var spotifyData = getSpotifyID(data.entities.urls);
 
         if (githubData.length > 0) {
             // console.log("---- githubData:");
@@ -70,12 +77,12 @@ function handleStream () {
             emitGithub(githubData);
         } 
         
-        if (spotifyData.length > 0 ) {
-            // console.log("---- SpotifyData:");
-            // console.log(data.text);
-            // console.log(spotifyData);
-            emitSpotify(spotifyData);
-        }
+        // if (spotifyData.length > 0 ) {
+        // console.log("---- SpotifyData:");
+        // console.log(data.text);
+        // console.log(spotifyData);
+        // emitSpotify(spotifyData);
+        // }
     }
 }
 
@@ -93,7 +100,7 @@ function emitData (sendObj) {
         obj.data = data;
         obj.date = new Date();
         
-        insert ("twitbuzzer", obj, function (err, docs) {
+        mongodb.insert ("twitbuzzer", obj, function (err, docs) {
             if (err) {
                 console.log("Insert error", err);
                 return;
@@ -120,10 +127,10 @@ function emitSpotify(spotifyData) {
         type: "spotify"
     };
 
-    // spotifyData.forEach(function (elm) {
-    //     obj.info = elm;
-    //     spotify.lookup({ type: elm[0], id: elm[1] }, emitData(obj));
-    // });
+    spotifyData.forEach(function (elm) {
+        obj.info = elm;
+        spotify.lookup({ type: elm[0], id: elm[1] }, emitData(obj));
+    });
 }
 
 function filter(regex) {
@@ -136,11 +143,11 @@ function filter(regex) {
 }
 
 function getSpotifyID (urls) {
-    return getID(urls, config.SPOTIFY_REGEX, filter(config.SPOTIFY_REGEX));
+    return getID(urls, config.app.SPOTIFY_REGEX, filter(config.app.SPOTIFY_REGEX));
 }
 
 function getGithubID (urls) {
-    return getID(urls, config.GITHUB_REGEX, filter(config.GITHUB_REGEX));
+    return getID(urls, config.app.GITHUB_REGEX, filter(config.app.GITHUB_REGEX));
 }
 
 function getID(urls, regex, filter) {
@@ -153,120 +160,6 @@ function getID(urls, regex, filter) {
     return id;
 }
 
-
-/*********************
- * MongoDB Handling 
- *********************/
-
-
-
-//
-// Opens connection to MongoDB database, authenticates, logs successful connection.
-//
-function initializeDb() {
-    mongoose.connection.on("open", function() {
-        console.log("Connected to MongoDB successfully!");
-    });
-    mongoose.connect("mongodb://" + config.DB_USER + ":" + config.DB_PASS + "@" + config.DB_URL + "/" + config.DB_NAME);   
-}
-
-
-//
-// Queries a MongoDB collection to retrieve data based on
-// properties supplied by json parameter.
-//
-function query (collectionIdent, json, callback) {
-    mongoose.connection.db.collection(collectionIdent, function (err, collection) {
-        collection.find(json).toArray(callback);
-    });
-}
-
-function count (collectionIdent, json, callback) {
-    mongoose.connection.db.collection(collectionIdent, function (err, collection) {
-        callback(err, collection.find(json).count());
-    });
-}
-
-function get (collectionIdent, json, callback) {
-    mongoose.connection.db.collection(collectionIdent, function (err, collection) {
-        var res = collection.group({"data.html_url": true}, {"type": "github"}, {count: 0}, function(doc, out){ out.count++; }, function(doc) {}, true, callback);
-    });
-}
-
-function mapReduce (collectionIdent, callback) {
-
-    urlMap = function() { //map function
-        emit(this.data.id, {
-                data: { // Github Model
-                    language: this.data.language,
-                    html_url: this.data.html_url,
-                    name: this.data.name,
-                    description: this.data.description,
-                    owner: {
-                        avatar_url: this.data.owner.avatar_url,
-                        login: this.data.owner.login,
-                        url: this.data.owner.url
-                    },
-                    forks: this.data.forks,
-                    watchers: this.data.watchers
-                },
-                count: 1,
-                date: this.date
-            }); //sends the url 'key' and a 'value' of 1 to the reduce function
-    } 
-
-    urlReduce = function(key, values) { //reduce function
-        var reduced = {"data":{}, count: 0, "date": 0};
-        reduced.date = values[0].date;
-        reduced.data = values[0].data;
-
-        for (var i in values) {
-            reduced.count += values[i].count;
-        }
-
-        return reduced;
-    };
-
-    mongoose.connection.db.collection(collectionIdent, function (err, collection) {
-
-        if(err) console.log( err );
-
-        console.log("Collection:");
-        console.log(collection);
-
-        // current date
-        var now = new Date();
-        // 7 days earlier
-        now.setDate(now.getDate()-100);
-        var query = {'date' : { $gt: now }};
-        var options = {  query: query };
-
-        collection.mapReduce(urlMap.toString(), urlReduce.toString(), options, function (err, collection) {
-            if(err) console.log( err );
-
-            console.log("Collection:");
-            console.log(collection);
-
-            collection.find({}).sort({'value.count': -1}).limit(10).toArray(callback);
-        });
-    });
-
-}
-
-function remove (collectionIdent, json, callback) {
-    mongoose.connection.db.collection(collectionIdent, function (err, collection) {
-        collection.remove(json);
-    });
-}
-//
-// Inserts into a MongoDB collection and returns inserted data
-//
-function insert (collectionIdent, json, callback) {
-    mongoose.connection.db.collection(collectionIdent, function (err, collection) {
-        collection.insert(json);
-        callback(err, collection);
-    });
-}
-
-
-
+/*****************************************************
+ *              END Tweet Stream handling            *
+ *****************************************************/
