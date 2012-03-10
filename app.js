675a0097eeec71606e9a034e875090242c1c6a92
Mikael Brevik <mikaelb@mikaelb.net>
2012-03-10 02:02:20 +0100

    Heroku

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var express       = require('express')
   , routes        = require('./routes')
   , githubrepo    = require('github-repo')
   , io            = require('socket.io').listen(app)
-  , ts            = require('./lib/repo-streamer').RepoStreamer
+  , ts            = require('./lib/repo-streamer.mongoose').RepoStreamer
   , RepoProvider  = require('./lib/repo-provider').RepoProvider;
 
 
